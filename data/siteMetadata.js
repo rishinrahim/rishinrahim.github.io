326cc861c5932fa80cbcad50f98840602d7dad15
Rishin Rahim <rishin07@gmail.com>
2025-02-24 21:18:41 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Rishin\'s Blog',
   author: 'Rishin Rahim',
   headerTitle: 'Rishin Rahim',
-  description: 'A mix of my thoughts and insights on machine learning and life in general. While most of it remains relevant, some entries reflect what I knew at the time. The goal is to share and learn, not to mislead.',
+  description: 'Hi, this is Rishin. I’m documenting my thoughts and insights on machine learning and life in general. While most of it remains relevant, some entries reflect what I knew at the time. The goal is to share and learn, not to mislead.',
   language: 'en-us',
   theme: 'light', // system, dark or light
   siteUrl: 'https://rishinrahim.com',
@@ -52,30 +52,30 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
-    giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      // theme example: light, dark, dark_dimmed, dark_high_contrast
-      // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'transparent_dark',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
-    },
+    // provider: 'giscus', // supported providers: giscus, utterances, disqus
+    // giscusConfig: {
+    //   // Visit the link below, and follow the steps in the 'configuration' section
+    //   // https://giscus.app/
+    //   repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+    //   repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+    //   category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+    //   categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+    //   mapping: 'pathname', // supported options: pathname, url, title
+    //   reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+    //   // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+    //   metadata: '0',
+    //   // theme example: light, dark, dark_dimmed, dark_high_contrast
+    //   // transparent_dark, preferred_color_scheme, custom
+    //   theme: 'light',
+    //   // theme when dark mode
+    //   darkTheme: 'transparent_dark',
+    //   // If the theme option above is set to 'custom`
+    //   // please provide a link below to your custom theme css file.
+    //   // example: https://giscus.app/themes/custom_example.css
+    //   themeURL: '',
+    //   // This corresponds to the `data-lang="en"` in giscus's configurations
+    //   lang: 'en',
+    // },
   },
   search: {
     provider: 'kbar', // kbar or algolia
