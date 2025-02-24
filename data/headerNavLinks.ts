85b4d401bb5729a78811e53ac1d7c658c632413d
Rishin Rahim <rishin07@gmail.com>
2025-02-24 21:18:38 +0530

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/books', title: 'Books' },
-  // { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
+  // { href: '/Travels', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
